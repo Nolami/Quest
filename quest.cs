3a9936cd52316fbfdb3f8585800d4f50159f6f98
Nolami <deata7660@gmail.com>
2024-02-04 18:55:18 +0200

    this was cancer

--- quest.cs
+++ quest.cs
@@ -16,7 +16,6 @@ class Program
         string month = Console.ReadLine();
         Console.WriteLine();
         
-
         if (date == 19 && month.ToLower() == "september")
         {
             Console.WriteLine("Good job, Dear");
@@ -36,28 +35,46 @@ class Program
     {
         while (true)
         {
-        Console.WriteLine("When is my birthday? ");
-        Console.WriteLine();
-        Console.Write("Insert the day here: ");
-        int date = int.Parse(Console.ReadLine());
-        Console.WriteLine();
-        Console.Write("Insert the month here: ");
-        string month = Console.ReadLine();
-        Console.WriteLine();
-
-        
-
-        if (date == 4 && month.ToLower() == "december")
-        {
-            Console.WriteLine("Good job, Dear, I'm proud of you");
+            Console.WriteLine("When is my birthday? ");
             Console.WriteLine();
+            
+            Console.Write("Insert the day here: ");
+            int date = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            
+            Console.Write("Insert the month here: ");
+            string month = Console.ReadLine();
+            Console.WriteLine();
+
+            if (date == 4 && month.ToLower() == "december")
+            {
+                Console.WriteLine("Good job, Dear, I'm proud of you");
+                Console.WriteLine();
                 break;
+            }
+            else
+            {
+                Console.WriteLine("It's fine dear, try again");
+                Console.WriteLine();
+            }
+        }
+    }
+    
+    static void gues()
+    {
+        Console.WriteLine("Will you be my valentine? ");
+        Console.WriteLine();
+        Console.Write("Yes or No: ");
+        string que = Console.ReadLine();
+        Console.WriteLine();
+        
+        if (que.ToLower() == "yes")
+        {
+            Console.WriteLine("Yippee.");
         }
         else
         {
-            Console.WriteLine("It's fine dear, try again");
-            Console.WriteLine();
-        }
+            Console.WriteLine("Awh :(");
         }
     }
 
@@ -66,6 +83,6 @@ class Program
 
         val();
         valr();
-
+        gues();
     }
 }
